27625670b3917993ee96b346aa3b1bc7e69a465c
johncambefort <jcambefort@middlebury.edu>
2019-11-22 15:14:54 -0500

    Upload current boot file

---
+++ ESP32-code/boot.py
@@ -0,0 +1,21 @@
+import network
+
+WIFI_SSID = 'MiddleburyGuest'
+
+def connect():
+    """ Setup a connection """
+    sta_if = network.WLAN(network.STA_IF)  # Configure as station
+
+    if not sta_if.isconnected():  # If it is not connected
+        print('Connecting to ' + WIFI_SSID)
+        sta_if.active(True)
+        sta_if.connect(WIFI_SSID)
+        while not sta_if.isconnected():
+            print('.', end='');
+    else:  # If we are already connected
+        print('Already connected to ' + sta_if.config('essid'))
+
+    # Show the network configuration
+    print('network config:', sta_if.ifconfig())
+
+connect()
