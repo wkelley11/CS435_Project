cd090df740fb6d0d770e9a5eb97d6867854b9033
johncambefort <jcambefort@middlebury.edu>
2019-12-02 15:39:50 -0500

    Create getWeather fct, returns tuple

--- get_weather.py
+++ get_weather.py
@@ -2,45 +2,47 @@
 import urequests
 import json
 
-# api-endpoint
-base_url = "http://openweathermap.org/data/2.5/weather?"
+# Returns a tuple:
+# - current temperature
+# - weather description
+def getWeather():
 
-# api key
-api_key = "1140ddfb29b9a42151d9a8b9ef40f27a"
+    # api-endpoint
+    base_url = "http://openweathermap.org/data/2.5/weather?"
 
-# location
-location = "Middlebury,us"
+    # api key
+    api_key = "1140ddfb29b9a42151d9a8b9ef40f27a"
 
-# complete url address
-complete_url = base_url + "/appid=" + api_key + "&q=" + location
-#print(complete_url)
+    # location
+    location = "Middlebury,us"
 
-# send get request and saving the response as response object
-response = urequests.get(complete_url)
+    # complete url address
+    complete_url = base_url + "/appid=" + api_key + "&q=" + location
+    #print(complete_url)
 
-# get data in json format
-data = response.json()
-#print(data)
+    # send get request and saving the response as response object
+    response = urequests.get(complete_url)
 
-#format and print desired data from json file
-if data["cod"] != "404" | "401":
-    formated_info = data["main"]
+    # get data in json format
+    data = response.json()
+    #print(data)
 
-    #get current temp
-    current_temperature = formated_info["temp"]
+    #format and print desired data from json file
+    if data["cod"] != "404" | "401":
+        formated_info = data["main"]
 
-    #get current pressure
-    current_pressure = formated_info["pressure"]
+        #get current temp
+        current_temperature = formated_info["temp"]
 
-    #get current hum
-    current_humidity = formated_info["humidity"]
+        #get current pressure
+        current_pressure = formated_info["pressure"]
 
-    #get summary
-    summary = data["weather"]
-    description = summary[0]["description"]
+        #get current hum
+        current_humidity = formated_info["humidity"]
 
-    #print weather data
-    print("Temperature: " +
-              str(current_temperature) +
-          "Weather: " +
-              str(description))
+        #get summary
+        summary = data["weather"]
+        description = summary[0]["description"]
+
+        #return weather data: temperature and short weather description (e.g. "sunny")
+        return(str(current_temperature), str(description))
