b80fccfffc220d123889d65814bae54379e16e0e
johncambefort <jcambefort@middlebury.edu>
2019-12-03 21:03:58 -0500

    Implement string size expansion

--- ESP32-code/display/size_convert.py
+++ ESP32-code/display/size_convert.py
@@ -1,7 +1,29 @@
 # Python script to modify the display size of a given line of text.
 # An expansion factor of 2 would double the size of the text (4 times as many pixels occupied).
 
-def bigText(oled, bits, factor, x, y):
+from font_petme128_8x8 import alphabet
+
+
+# Input:
+# - oled object
+# - message to display
+# - factor (int) by which to expand the string
+# - x and y top-left start coordinates of 1st char
+# - spacing to put between each char
+def bigText(oled, message, factor, x_start, y_start, spacing):
+
+    # iterate over each char the string
+    x = x_start
+    y = y_start
+    for char in message:
+        asc = ord(char)
+        byte = alphabet[asc - 32]
+        bigChar(oled, byte, factor, x, y)
+        x += (factor * 8)
+        # y does not change
+
+
+def bigChar(oled, bits, factor, x, y):
 
     for i in range(len(bits)): # For every row of bits, in matrix bits
         for b in range(8): # for each bit in a byte
