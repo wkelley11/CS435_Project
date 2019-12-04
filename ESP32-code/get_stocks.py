8119a335751ce116cc6816ace820394a6a12163d
johncambefort <jcambefort@middlebury.edu>
2019-12-04 10:01:26 -0500

    Implement getStocks and getExchange

---
+++ ESP32-code/get_stocks.py
@@ -0,0 +1,55 @@
+# Use Alphavantage.co to get financial information
+
+import urequests
+import json
+
+# Returns current
+def getStocks(stock):
+    # Alphavantage credentials
+    api_key = "NRKWC04SMTNZ17H2"
+
+    # Alphavantage API request url
+    base_url = "https://www.alphavantage.co/query?"
+
+    function = "function=TIME_SERIES_DAILY"
+    symbol = "symbol=" + stock # get the current stock value
+
+    complete_url = base_url + function + "&" + symbol + "&apikey=" + api_key
+
+    response = urequests.get(complete_url)
+
+    data = response.json()
+
+    #if(response.status_code != "404"):
+    times = data["Time Series (Daily)"]
+
+    today = times["2019-12-04"]
+
+    current_value = today.get("4. close")
+
+    return current_value
+
+# Returns current from_currency to to_currency conversion rate
+def getExchange(from_currency, to_currency):
+
+    # Alphavantage credentials
+    api_key = "NRKWC04SMTNZ17H2"
+
+    # Alphavantage API request url
+    base_url = "https://www.alphavantage.co/query?"
+
+    function = "function=CURRENCY_EXCHANGE_RATE"
+    from_c = "from_currency=" + from_currency
+    to_c = "to_currency=" + to_currency
+
+    complete_url = base_url + function + "&" + from_c + "&" + to_c + "&apikey=" + api_key
+
+    response = urequests.get(complete_url)
+
+    data = response.json()
+
+    all = data.get("Realtime Currency Exchange Rate")
+
+    er = all.get("5. Exchange Rate")
+
+    return er
